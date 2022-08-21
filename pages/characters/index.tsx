17f917bb38a9e47b9bdf00231497eab024b1dc28
Dave Frame <dframe22@gmail.com>
2022-08-20 23:34:37 -0600

    big style shift

---
+++ pages/characters/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Characters = () => {
+  return (
+    <div className="container">
+      <h1>Characters</h1>
+      <div className="screen">
+        <div className="screen__menu">
+          <div className="screen__menu__item">
+            One
+          </div>
+          <div className="screen__menu__item">
+            Two
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Characters;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,16 @@ export default function Index(){
     <>
       <Head>
         <title>Home -- Dave</title>
-        <link rel="icon" href="/davicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container">
         <div className="row">
-          <section className="welcome">
-            <h1 className="title">Well, hello there!</h1>
+          <section className="page">
+            <h1 className="page__label">Well, hello there!</h1>
             <p className="body">I see you've found my homepage. I'm sorry, I wasn't expecting company yet, but feel free to look around...</p>
           </section>
         </div>
       </div>
-    </> 
+    </>
   )
 }
